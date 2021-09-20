a063eec581713b82448cab4d4e567157a18bdf0e
Indrajeet Patil <patilindrajeet.science@gmail.com>
2021-09-20 14:53:28 +0200

    remove obsolete existsDimensions; format more to 80-char

--- R/utilities-dimensions.R
+++
@@ -1,8 +0,0 @@
-#' Dimension existence
-#'
-#' @param dimension String representation of the dimension.
-#' @details Deprecated, use ospsuite::ospDimensions instead. Check if the provided dimension is supported.
-#' @export
-existsDimension <- function(dimension) {
-  ospsuite::hasDimension(dimension = dimension)
-}
