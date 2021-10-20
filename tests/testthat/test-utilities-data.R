35a04657212d29ca03dbb90f3983e411ca6544e4
Johanna Eitel <johanna.eitel@esqlabs.com>
2021-10-20 22:45:30 +0200

    more changes for issue 97

--- tests/testthat/test-utilities-data.R
+++ tests/testthat/test-utilities-data.R
@@ -38,3 +38,39 @@ test_that("It converts a non numerics LLOQ to zero to NA", {
   string <- c("21", "one", "<5", "-21")
   expect_equal(stringToNum(string), c(21, NA, 0, -21))
 })
+
+## context("calculateMeans")
+
+test_that("It can save the results to a new file", {
+  path <- getTestDataFilePath("CompiledDataSet.xlsx")
+  sheet <- "Test_MeanData"
+  calculateMeans(path, sheet)
+  newPath <- getTestDataFilePath("CompiledDataSet_mean.xlsx")
+  expect_true(file.exists(newPath))
+
+  meanData <- readExcel(newPath)
+  expect_setequal(
+    names(meanData),
+    c(
+      "Study Id", "Patient Id", "Organ", "Compartment", "Species",
+      "Gender", "Dose [unit]", "Molecule", "MW", "Time [h]",
+      "Fraction [%]", "Error [%]", "Route", "Group Id"
+    )
+  )
+  file.remove(newPath)
+})
+
+test_that("It throws an error when required columns are missing in the file", {
+  path <- getTestDataFilePath("CompiledDataSet.xlsx")
+  sheet <- "TestSheet_1"
+  expect_error(calculateMeans(path, sheet))
+})
+
+test_that("It throws an error when the sheet is empty (except for column names)", {
+  path <- getTestDataFilePath("CompiledDataSet.xlsx")
+  sheet <- "WrongSheet"
+  expect_error(
+    calculateMeans(path, sheet),
+    paste("The provided sheet from the file", path, "does not contain any data.")
+  )
+})
