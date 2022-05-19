208fb2cb30312c2259b1298551106277c412f7d8
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-05-19 09:41:29 +0200

    Don't create custom log axis ticks and labels (#236)
    Closes #231
    
    We don't do this for any of the other log axes; not sure why we were doing so only in this one instance. Let ggplot2 defaults take care of this than a custom solution that is bound to misbehave in some contexts.

--- R/sensivitityTimeProfiles.R
+++ R/sensivitityTimeProfiles.R
@@ -158,11 +158,7 @@ sensitivityTimeProfiles <- function(sensitivityCalculation,
   }
 
   if (yAxisLog) {
-    plot <- plot +
-      scale_y_log10(
-        breaks = scales::trans_breaks("log10", function(x) 10^x),
-        labels = scales::trans_format("log10", scales::math_format(10^.x))
-      )
+    plot <- plot + scale_y_log10()
   }
 
   plot +
