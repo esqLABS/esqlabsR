842915d079e5cd88a966760c84ffc0ffe10fb6a8
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-07-26 17:12:41 +0200

    Update test-utilities-file.R

--- tests/testthat/test-utilities-file.R
+++ tests/testthat/test-utilities-file.R
@@ -1,33 +1,33 @@
 ##  context("sourceAll")
 
-# test_that("`sourceAll()` sources all files in the directory", {
-#   skip_if_not_installed("withr")
-#
-#   withr::with_tempdir(
-#     code = {
-#       f1 <- file.create("f1.R")
-#       f2 <- file.create("f2.R")
-#       writeLines("var1 <- 1 + 1", "f1.R")
-#       writeLines("var2 <- paste0('a', 'b')", "f2.R")
-#
-#       sourceAll(".")
-#
-#       expect_true(exists("var1"))
-#       expect_true(exists("var2"))
-#     }
-#   )
-# })
+test_that("`sourceAll()` sources all files in the directory", {
+  skip_if_not_installed("withr")
+
+  withr::with_tempdir(
+    code = {
+      f1 <- file.create("f1.R")
+      f2 <- file.create("f2.R")
+      writeLines("var1 <- 1 + 1", "f1.R")
+      writeLines("var2 <- paste0('a', 'b')", "f2.R")
+
+      sourceAll(".")
+
+      expect_true(exists("var1"))
+      expect_true(exists("var2"))
+    }
+  )
+})
 
 ##  context("pathFromClipboard")
 
-test_that("`pathFromClipboard()` converts paths as expected", {
-  skip_if_not_installed("clipr")
-
-  path <- "C:\\Users\\Documents"
-  clipr::write_clip(path, allow_non_interactive = TRUE)
-
-  expect_equal(
-    pathFromClipboard(),
-    "C:/Users/Documents"
-  )
-})
+# test_that("`pathFromClipboard()` converts paths as expected", {
+#   skip_if_not_installed("clipr")
+#
+#   path <- "C:\\Users\\Documents"
+#   clipr::write_clip(path, allow_non_interactive = TRUE)
+#
+#   expect_equal(
+#     pathFromClipboard(),
+#     "C:/Users/Documents"
+#   )
+# })
