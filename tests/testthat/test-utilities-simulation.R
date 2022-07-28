120c04167f0ac565795739cc66d62ee89bf65385
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-07-28 11:59:24 +0200

    Additional tests for utilities (#287)
    * Additional tests for utilities
    
    * test for `compareSimulationParameters()`
    
    * tweak tests for settings
    
    * Test for `sampleRandomValue()`
    
    * Address review comments
    
    * remove `expect_s3_class()` for R6

---
+++ tests/testthat/test-utilities-simulation.R
@@ -0,0 +1,22 @@
+## context("initializeSimulation")
+
+test_that("`initializeSimulation()` loads a simulation at the minimum", {
+  simulation <- loadSimulation(system.file("extdata", "simple.pkml", package = "ospsuite"))
+  initializeSimulation(simulation, steadyStateTime = TRUE)
+  simulationResults <- runSimulation(simulation)
+  expect_true(isOfType(simulationResults, "SimulationResults"))
+})
+
+## context("compareSimulationParameters")
+
+test_that("`compareSimulationParameters()` produces no differences with identical simulations", {
+  simPath <- system.file("extdata", "simple.pkml", package = "ospsuite")
+  sim1 <- loadSimulation(simPath)
+  sim2 <- loadSimulation(simPath)
+
+  res <- compareSimulationParameters(sim1, sim2)
+  expect_equal(
+    res,
+    list(In1NotIn2 = list(), In2NotIn1 = list(), Different = list())
+  )
+})
