7d84524d57b7954e9dfb0db48830f564123fb932
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-08-01 15:04:12 +0200

    use github version

--- tests/testthat/test-utilities-steady-state.R
+++ tests/testthat/test-utilities-steady-state.R
@@ -1,4 +1,4 @@
-test_that("`exportSteadyStateToXLS` generates excel sheet", {
+test_that("`exportSteadyStateToXLS` generates excel sheet path", {
   withr::with_tempdir(
     code = {
       simFilePath <- system.file("extdata", "Aciclovir.pkml", package = "ospsuite")
