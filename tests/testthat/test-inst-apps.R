1f8d656c49d8d1674eb0e5993a82778834f8d72a
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-08-11 22:12:31 +0200

    Run tests for Shiny apps

---
+++ tests/testthat/test-inst-apps.R
@@ -0,0 +1,18 @@
+library(shinytest2)
+
+test_that("`UnitConverter` Shiny app works", {
+  skip_on_cran()
+
+  appdir <- system.file(package = "esqlabsR", "UnitConverter")
+  test_app(appdir)
+})
+
+
+test_that("`FunctionVisualizer` Shiny app works", {
+  skip_on_cran()
+
+  appdir <- system.file(package = "esqlabsR", "FunctionVisualizer")
+  test_app(appdir)
+})
+
+
