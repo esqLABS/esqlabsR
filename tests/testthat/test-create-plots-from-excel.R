58b6014936a9126c3da980807f2fa933ff2a8a19
Pavel Balazki <pavel.balazki@gmail.com>
2022-12-07 10:47:47 +0100

    Add initial test file for plots-from-excel

---
+++ tests/testthat/test-create-plots-from-excel.R
@@ -0,0 +1,38 @@
+projectConfiguration <- createDefaultProjectConfiguration(file.path("..", "data", "ProjectConfiguration_forTests.xlsx"))
+
+# Define which scenarios to run
+scenarioNames <- c("TestScenario")
+outputPaths <- "Organism|PeripheralVenousBlood|Aciclovir|Plasma (Peripheral Venous Blood)"
+
+# Create `ScenarioConfiguration` objects from excel files
+scenarioConfigurations <- readScenarioConfigurationFromExcel(
+  scenarioNames = scenarioNames,
+  projectConfiguration = projectConfiguration
+)
+
+# Set output paths for each scenario
+for (scenarioConfiguration in scenarioConfigurations) {
+  scenarioConfiguration$outputPaths <- outputPaths
+}
+
+# Run scenarios
+simulatedScenarios <- runScenarios(
+  scenarioConfigurations = scenarioConfigurations,
+  customParams = NULL, saveSimulationsToPKML = FALSE
+)
+
+########### Load observed data - data template v10########
+# Which sheets to load
+dataSheets <- "Laskin 1982.Group A"
+observedData <- esqlabsR::loadObservedData(
+  projectConfiguration = projectConfiguration,
+  sheets = dataSheets
+)
+
+test_that("No plots are defined in the excel sheet", {
+plots <- createPlotsFromExcel(simulatedScenarios = simulatedScenarios,
+observedData = observedData,
+projectConfiguration = projectConfiguration,
+stopIfNotFound = TRUE)
+}
+)
