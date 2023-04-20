bf8dc05d67040722a4f6b1ed7f3f69787cc5909a
Pavel Balazki <pavel.balazki@gmail.com>
2023-04-20 16:40:29 +0200

    Update test snaps

--- tests/testthat/test-utilities-scenario-configuration.R
+++ tests/testthat/test-utilities-scenario-configuration.R
@@ -119,7 +119,7 @@ test_that("It creates all scenarios if no name is defined", {
   scenarioConfigurations <- readScenarioConfigurationFromExcel(
     projectConfiguration = projectConfiguration
   )
-  expect_equal(names(scenarioConfigurations), c("TestScenario", "TestScenario2", "PopulationScenario"))
+  expect_equal(names(scenarioConfigurations), c("TestScenario", "TestScenario2", "PopulationScenario", "PopulationScenarioFromCSV"))
 })
 
 test_that("It correctly applies a custom function", {
