7a5c664ab496839f1cd686f578ba7fe88fa0fda4
Felix MIL <felix.mil@esqlabs.com>
2023-04-21 09:31:43 +0200

    Add function to check if suggested shiny related packages are installed before launching Apps

--- R/shiny-app-launcher.R
+++ R/shiny-app-launcher.R
@@ -1,9 +1,9 @@
 #' Start function visualizer
 #' @details Starts a shiny app for visualizing of function values.
-#' @import shiny
-#' @import shinyjs
+
 #' @export
 startFunctionVisualizer <- function() {
+  checkShinyPackages()
   appDir <- system.file("FunctionVisualizer", package = "esqlabsR")
   shiny::runApp(appDir, display.mode = "normal")
 }
@@ -12,6 +12,13 @@ startFunctionVisualizer <- function() {
 #' @details Starts a shiny app for computing unit conversions.
 #' @export
 startUnitConverter <- function() {
+  checkShinyPackages()
   appDir <- system.file("UnitConverter", package = "esqlabsR")
   shiny::runApp(appDir, display.mode = "normal")
+}
+
+#' Check if shiny, shinyjs packages are installed
+checkShinyPackages <- function() {
+  rlang::check_installed("shiny", reason = "to launch this shiny App")
+  rlang::check_installed("shinyjs", reason = "to launch this shiny App")
 }
