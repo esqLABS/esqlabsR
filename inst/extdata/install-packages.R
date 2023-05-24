3b6e30fc7f25f8504f1d236e085e7ba6b2e18913
Felix MIL <felix.mil@esqlabs.com>
2023-05-24 11:54:54 +0200

    Refactor functions + add project initialization step

--- inst/extdata/install-packages.R
+++ inst/extdata/install-packages.R
@@ -1,124 +1,183 @@
-# install dependencies
-installationDeps <- function(installOption) {
-  cat("Installing installation dependencies.\n")
-  
-  # if user chose local installation, install renv and initialize it
-  if (installOption == 1) {
-    install.packages("renv")
-    renv::init(bare = TRUE, restart = FALSE)
-  }
-  install.packages("pkgbuild", quiet = TRUE)
-  install.packages("cli", quiet = TRUE)
-  install.packages("remotes", quiet = TRUE)
-  install.packages("rstudioapi", quiet = TRUE)
+install_script_deps <- function(install_option) {
 
+  # install dependencies required for this script
+
+  cat("Installing prerequisite dependencies.\n")
+
+  # if user chose local installation, install renv and initialize it
+  if (install_option == 1) {
+    install.packages("renv")
+    require(renv)
+    init(bare = TRUE, restart = FALSE)
+  }
+
+  # Declare packages
+  packages <- c("cli","remotes","rstudioapi","pkgbuild")
+
+  # Loop through each package
+  for (package in packages) {
+
+    # Install package
+    # Note: `installed.packages()` returns a vector of all the installed packages
+    if (!require(package,character.only = T, quietly = T)) {
+      # Install it
+      install.packages(
+        package,
+        dependencies = TRUE
+      )
+    }
+
+    # Load package
+    # Note: `.packages()` returns a vector of all the loaded packages
+    if (!(package %in% .packages())) {
+      # Load it
+      library(
+        package,
+        character.only = TRUE
+      )
+    }
+  }
+
+  cli_alert_success(text = "Prerequesite dependencies available.")
 }
 
-# check if rtools is installed
-checkRtools <- function() {
+check_RTools <- function() {
+  cli_progress_step("Verifying RTools installation")
   if (!pkgbuild::find_rtools()) {
-    cli::cli_alert_danger("Rtools is not installed. Please install it before continuing.")
-    cli::cli_alert_info("You can download Rtools from https://cran.r-project.org/bin/windows/Rtools/")
-    cli::cli_alert_info("Please install the latest version of Rtools.")
-    cli::cli_alert_info("After installing Rtools, please restart R and run this script again.")
+    cli_alert_danger("Rtools is not installed. Please install it before continuing.")
+    cli_alert_info("You can download Rtools from https://cran.r-project.org/bin/windows/Rtools/")
+    cli_alert_info("Please install the latest version of Rtools.")
+    cli_alert_info("After installing Rtools, please restart R and run this script again.")
     stop("Rtools is not installed.")
   }
 }
 
-# Get PKSim minimal
-getPKSimMinimal <- function() {
-  download.file("https://ci.appveyor.com/api/projects/open-systems-pharmacology-ci/ospsuite-r/artifacts/pksim_minimal.zip",
-                destfile = "pksim_minimal.zip"
-  )
-  unzip("pksim_minimal.zip", exdir = "PKSim")
-  file.remove("pksim_minimal.zip")
+get_PKSim_Minimal <- function(install_option) {
+
+  # Only for local installation
+  if (install_option == 1) {
+
+    cli_progress_step("Getting minimal version of PKSim.")
+
+    download.file("https://ci.appveyor.com/api/projects/open-systems-pharmacology-ci/ospsuite-r/artifacts/pksim_minimal.zip",
+                  destfile = "pksim_minimal.zip"
+    )
+    unzip("pksim_minimal.zip", exdir = "PKSim")
+    file.remove("pksim_minimal.zip")
+
+    cli_progress_step("Verifying PKSim installation")
+    # test if PK-Sim.R dll can be loaded
+    initPKSim()
+
+    snapshot(prompt = FALSE)
+
+  }
+
+
 }
 
-# Run test simulation
-runTestSimulation <- function() {
-  sourceAll(file.path(getwd(), "utils"))
-  sourceAll(file.path(getwd(), "InputCode"))
-  sourceAll(file.path(getwd(), "Scenarios"))
-  sourceAll(file.path(getwd(), "TransferFunctions"))
-  projectConfiguration <- createDefaultProjectConfiguration()
-  # Define which scenarios to run
-  scenarioNames <- c("TestScenario")
-  # Set scenario names to NULL if you want to simulate all scenarios defined in the
-  # excel file
-  # scenarioNames <- NULL
+get_esqlabsR <- function(){
+  # package installation
+  cli_progress_step("Installing esqlabsR and dependencies")
 
-  # Create `ScenarioConfiguration` objects from excel files
-  scenarioConfigurations <- readScenarioConfigurationFromExcel(
-    scenarioNames = scenarioNames,
-    projectConfiguration = projectConfiguration
-  )
-  simulatedScenarios <- runScenarios(
-    scenarioConfigurations = scenarioConfigurations,
-    customParams = NULL, saveSimulationsToPKML = TRUE
-  )
-}
-
-# Main installation script
-installEsqLabsR <- function() {
-  # Display a menu asking if user wants to install packages in local env or  globally
-  installOption <- utils::menu(c("In local environment (available for one project)", "In Glocal environment (available for all projects)"),
-                               title = "Where do you want to install {esqlabsR} and other packages?"
-  )
-
-  installationDeps(installOption)
-
-  checkRtools()
-
-
-  cli::cli_progress_step("Installing esqlabsR and dependencies")
   install.packages("https://github.com/Open-Systems-Pharmacology/rClr/releases/download/v0.9.2/rClr_0.9.2.zip",
                    repos = NULL,
                    type = "binary",
                    quiet = TRUE
   )
-  remotes::install_github("esqLABS/esqlabsR",
+  install_github("esqLABS/esqlabsR",
                           quiet = TRUE,
                           build = TRUE
   )
+}
 
-  # Only for local installation
-  if (installOption == 1) {
-
-    cli::cli_progress_step("Getting minimal version of PKSim.")
-    getPKSimMinimal()
-
-    cli::cli_progress_step("Verifying PKSim installation")
-    # test if PK-Sim.R dll can be loaded
-    ospsuite::initPKSim()
-
-    renv::snapshot(prompt = FALSE)
-  }
-
-  cli::cli_progress_step("Loading esqlabsR")
+load_esqlabsR <- function(){
+  cli_progress_step("Loading esqlabsR")
   # test if installed packages can be loaded
   library("esqlabsR")
+}
 
+run_test_simulation <- function() {
+  # Run test simulation to verify installation
 
-  cli::cli_progress_step("Running test simulation.")
-  # run test simulations if the working directory path ends with "Code"
-  if (endsWith(getwd(), "Code")) {
-    runTestSimulation()
-    cli::cli_progress_done()
-  } else {
-    cli::cli_alert_warning("Test simulation files could not be found.")
-    cli::cli_progress_done(result = "failed")
-  }
+  cli_progress_step("Running test simulation.")
 
-  cli::cli_alert_success("Installation successful.")
+  tryCatch(expr = {
+    require(esqlabsR)
 
-  # if running in RStudio, restart R session
-  if (rstudioapi::isAvailable()) {
-    cli::cli_alert_info("Restarting R session.")
-    rstudioapi::restartSession()
-  } else {
-    cli::cli_alert_info("Please restart R to complete the installation.")
+    suppressWarnings({
+      projectConfiguration <- createDefaultProjectConfiguration(path = esqlabsR:::test_ProjectConfiguration())
+    })
+    # Define which scenarios to run
+    scenarioNames <- c("TestScenario")
+    # Set scenario names to NULL if you want to simulate all scenarios defined in the
+    # excel file
+    # scenarioNames <- NULL
+
+    # Create `ScenarioConfiguration` objects from excel files
+    scenarioConfigurations <- readScenarioConfigurationFromExcel(
+      scenarioNames = scenarioNames,
+      projectConfiguration = projectConfiguration
+    )
+
+    scenarios <- createScenarios(scenarioConfigurations)
+
+    simulatedScenarios <- runScenarios(scenarios = scenarios)
+
+    cli_progress_done(result = "done")
+    cli_alert_success("Installation successful.")
+
+  },
+  error = function(error){
+    cli_progress_done(result = "failed")
+    cli_alert_danger(text = paste("Simulation test failed: ", error))
+  })
+}
+
+initialize_project <- function(){
+  initialize_option <- utils::menu(c("Yes", "No"),
+                               title = "Do you want to initialize project folder structure ?"
+  )
+  if (initialize_option == 1) {
+    init_project()
   }
 }
 
+restart_rstudio <- function() {
+  rm(list = ls(envir = .GlobalEnv), envir = .GlobalEnv)
+  # if running in RStudio, restart R session
+  if (isAvailable()) {
+    cli_alert_info("Restarting R session.")
+    restartSession(command = "library(esqlabsR)")
+  } else {
+    cli_alert_info("Please restart R to complete the installation.")
+  }
+}
+
+# Main installation script
+setup_esqlabsR <- function() {
+  # Display a menu asking if user wants to install packages in local env or  globally
+  install_option <- utils::menu(c("In local environment (available for one project)", "In Glocal environment (available for all projects)"),
+                               title = "Where do you want to install {esqlabsR} and other packages?"
+  )
+
+  install_script_deps(install_option)
+
+  check_RTools()
+
+  get_PKSim_Minimal(install_option)
+
+  get_esqlabsR()
+
+  load_esqlabsR()
+
+  run_test_simulation()
+
+  initialize_project()
+
+  restart_rstudio()
+
+}
+
 # run installation function
-installEsqLabsR()
+setup_esqlabsR()
