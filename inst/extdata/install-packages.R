b3fac5a30af3430b6ad4490b09fd77c783a054ee
Felix MIL <felix.mil@esqlabs.com>
2023-05-25 10:25:40 +0200

    Add steps that the powershell script used to do i.e update ProjectConfiguration file with write DataFile and compoundPropertiesFile names

--- inst/extdata/install-packages.R
+++ inst/extdata/install-packages.R
@@ -2,7 +2,7 @@ install_script_deps <- function(install_option) {
 
   # install dependencies required for this script
 
-  cat("Installing prerequisite dependencies.\n")
+  cat("Install prerequisite dependencies.\n")
 
   # if user chose local installation, install renv and initialize it
   if (install_option == 1) {
@@ -42,7 +42,7 @@ install_script_deps <- function(install_option) {
 }
 
 check_RTools <- function() {
-  cli_progress_step("Verifying RTools installation")
+  cli_progress_step("Verify RTools installation")
   if (!pkgbuild::find_rtools()) {
     cli_alert_danger("Rtools is not installed. Please install it before continuing.")
     cli_alert_info("You can download Rtools from https://cran.r-project.org/bin/windows/Rtools/")
@@ -57,7 +57,7 @@ get_PKSim_Minimal <- function(install_option) {
   # Only for local installation
   if (install_option == 1) {
 
-    cli_progress_step("Getting minimal version of PKSim.")
+    cli_progress_step("Get minimal version of PKSim.")
 
     download.file("https://ci.appveyor.com/api/projects/open-systems-pharmacology-ci/ospsuite-r/artifacts/pksim_minimal.zip",
                   destfile = "pksim_minimal.zip"
@@ -65,7 +65,7 @@ get_PKSim_Minimal <- function(install_option) {
     unzip("pksim_minimal.zip", exdir = "PKSim")
     file.remove("pksim_minimal.zip")
 
-    cli_progress_step("Verifying PKSim installation")
+    cli_progress_step("Verify PKSim installation")
     # test if PK-Sim.R dll can be loaded
     initPKSim()
 
@@ -78,7 +78,7 @@ get_PKSim_Minimal <- function(install_option) {
 
 get_esqlabsR <- function(){
   # package installation
-  cli_progress_step("Installing esqlabsR and dependencies")
+  cli_progress_step("Install esqlabsR and dependencies")
 
   install.packages("https://github.com/Open-Systems-Pharmacology/rClr/releases/download/v0.9.2/rClr_0.9.2.zip",
                    repos = NULL,
@@ -86,13 +86,13 @@ get_esqlabsR <- function(){
                    quiet = TRUE
   )
   install_github("esqLABS/esqlabsR",
-                          quiet = TRUE,
-                          build = TRUE
+                 quiet = TRUE,
+                 build = TRUE
   )
 }
 
 load_esqlabsR <- function(){
-  cli_progress_step("Loading esqlabsR")
+  cli_progress_step("Load esqlabsR")
   # test if installed packages can be loaded
   library("esqlabsR")
 }
@@ -100,7 +100,7 @@ load_esqlabsR <- function(){
 run_test_simulation <- function() {
   # Run test simulation to verify installation
 
-  cli_progress_step("Running test simulation.")
+  cli_progress_step("Runn test simulation.")
 
   tryCatch(expr = {
     require(esqlabsR)
@@ -136,11 +136,43 @@ run_test_simulation <- function() {
 
 initialize_project <- function(){
   initialize_option <- utils::menu(c("Yes", "No"),
-                               title = "Do you want to initialize project folder structure ?"
+                                   title = "Do you want to initialize project folder structure ?"
   )
   if (initialize_option == 1) {
-    init_project()
+    init_project(destination = "../")
   }
+
+  update_project_conf_file()
+}
+
+update_project_conf_file <- function(){
+  proj_name <- get_project_name()
+
+  timevalues_file <-
+    list.files("../Data",
+               pattern = "*TimeValuesData.xlsx")[1]
+
+  compoundpropertiesinternal_file <-
+    list.files("../Data",
+               pattern = "*_Compound Properties \\(Internal\\).xlsx")[1]
+
+  project_configuration <-
+    createDefaultProjectConfiguration(path = "../ProjectConfiguration.xlsx")
+
+  project_configuration$dataFile <- timevalues_file
+
+  project_configuration$compoundPropertiesFile <- compoundpropertiesinternal_file
+
+  project_configuration$save()
+}
+
+get_project_name <- function(){
+  rproj_name <- list.files(pattern = ".Rproj")
+  proj_name <- sub(pattern = "_V.*?.Rproj",
+                   replacement = "",
+                   x = rproj_name)
+
+  return(proj_name)
 }
 
 restart_rstudio <- function() {
@@ -158,7 +190,7 @@ restart_rstudio <- function() {
 setup_esqlabsR <- function() {
   # Display a menu asking if user wants to install packages in local env or  globally
   install_option <- utils::menu(c("In local environment (available for one project)", "In Glocal environment (available for all projects)"),
-                               title = "Where do you want to install {esqlabsR} and other packages?"
+                                title = "Where do you want to install {esqlabsR} and other packages?"
   )
 
   install_script_deps(install_option)
