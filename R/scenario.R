9aab9ef149f4773fa495a1a5dead65e7e610a8d4
Pavel Balazki <pavel.balazki@gmail.com>
2023-07-04 13:48:36 +0200

    Scenario$population is always a Population object and not

--- R/scenario.R
+++ R/scenario.R
@@ -52,7 +52,7 @@ Scenario <- R6::R6Class(
     #' @field scenarioType Type of the scenario - individual or population. Read-only
     scenarioType = function(value) {
       if (missing(value)) {
-        if (!any(is.na(private$.population)) && isOfType(private$.population$population, "Population")) {
+        if (!any(is.na(private$.population)) && isOfType(private$.population, "Population")) {
           "Population"
         } else {
           "Individual"
@@ -204,6 +204,8 @@ Scenario <- R6::R6Class(
             sheet = "Demographics"
           )
           population <- createPopulation(populationCharacteristics = popCharacteristics)
+          # Create population returns a list, in cotrast to load population, where the object is returned!
+          population <- population$population
         }
       }
       private$.population <- population
@@ -220,6 +222,7 @@ Scenario <- R6::R6Class(
     #' units the values are in.
     #' @return A new `Scenario` object.
     initialize = function(scenarioConfiguration, customParams = NULL) {
+      browser()
       private$.scenarioConfiguration <- scenarioConfiguration
       private$.simulation <- private$.initializeFromConfiguration(customParams = customParams)
       private$.initializePopulationFromConfiguration()
