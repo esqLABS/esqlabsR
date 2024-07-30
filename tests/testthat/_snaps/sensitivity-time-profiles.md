0fdc68403925ba912fb7f875e9e12b0d7370a4a1
rengelke <rudolf.engelke@web.de>
2024-07-30 23:44:52 +0200

    add tests and snapshots for unit conversion

--- tests/testthat/_snaps/sensitivity-time-profiles.md
+++ tests/testthat/_snaps/sensitivity-time-profiles.md
@@ -25,7 +25,104 @@
 # sensitivityTimeProfiles correctly applies linear y-axis scaling
 
     Code
-      unlist(plotParams)
+      extractAxisRange(p)
     Output
+      $`Organism|PeripheralVenousBlood|Aciclovir|Plasma (Peripheral Venous Blood)`
+      $`Organism|PeripheralVenousBlood|Aciclovir|Plasma (Peripheral Venous Blood)`$xRange
+      [1]  -72 1512  -72 1512  -72 1512
+      
+      $`Organism|PeripheralVenousBlood|Aciclovir|Plasma (Peripheral Venous Blood)`$yRange
       [1]  -50.76 1065.86  -50.76 1065.86  -50.76 1065.86
+      
+      
+
+# sensitivityTimeProfiles plots correctly apply y-unit conversion for multiple paths
+
+    Code
+      extractAxisRange(p)
+    Output
+      $`Organism|Age`
+      $`Organism|Age`$xRange
+      [1]  -72 1512  -72 1512  -72 1512
+      
+      $`Organism|Age`$yRange
+      [1] 2.442 2.539 2.442 2.539 2.442 2.539
+      
+      
+      $`Organism|ArterialBlood|Plasma|Aciclovir`
+      $`Organism|ArterialBlood|Plasma|Aciclovir`$xRange
+      [1]  -72 1512  -72 1512  -72 1512
+      
+      $`Organism|ArterialBlood|Plasma|Aciclovir`$yRange
+      [1] -0.4633  5.8395 -0.4633  5.8395 -0.4633  5.8395
+      
+      
+      $`Organism|PeripheralVenousBlood|Aciclovir|Plasma (Peripheral Venous Blood)`
+      $`Organism|PeripheralVenousBlood|Aciclovir|Plasma (Peripheral Venous Blood)`$xRange
+      [1]  -72 1512  -72 1512  -72 1512
+      
+      $`Organism|PeripheralVenousBlood|Aciclovir|Plasma (Peripheral Venous Blood)`$yRange
+      [1] -8.534 -2.735 -8.534 -2.735 -8.534 -2.735
+      
+      
+
+# sensitivityTimeProfiles plots correctly apply y-unit conversion for multiple paths with `NULL`
+
+    Code
+      extractAxisRange(p)
+    Output
+      $`Organism|Age`
+      $`Organism|Age`$xRange
+      [1]  -72 1512  -72 1512  -72 1512
+      
+      $`Organism|Age`$yRange
+      [1] 1.363 1.460 1.363 1.460 1.363 1.460
+      
+      
+      $`Organism|ArterialBlood|Plasma|Aciclovir`
+      $`Organism|ArterialBlood|Plasma|Aciclovir`$xRange
+      [1]  -72 1512  -72 1512  -72 1512
+      
+      $`Organism|ArterialBlood|Plasma|Aciclovir`$yRange
+      [1] -9.463 -3.161 -9.463 -3.161 -9.463 -3.161
+      
+      
+      $`Organism|PeripheralVenousBlood|Aciclovir|Plasma (Peripheral Venous Blood)`
+      $`Organism|PeripheralVenousBlood|Aciclovir|Plasma (Peripheral Venous Blood)`$xRange
+      [1]  -72 1512  -72 1512  -72 1512
+      
+      $`Organism|PeripheralVenousBlood|Aciclovir|Plasma (Peripheral Venous Blood)`$yRange
+      [1] -8.534 -2.735 -8.534 -2.735 -8.534 -2.735
+      
+      
+
+# sensitivityTimeProfiles plots correctly apply y-unit conversion for multiple paths with single unit
+
+    Code
+      extractAxisRange(p)
+    Output
+      $`Organism|Age`
+      $`Organism|Age`$xRange
+      [1]  -72 1512  -72 1512  -72 1512
+      
+      $`Organism|Age`$yRange
+      [1] 1.363 1.460 1.363 1.460 1.363 1.460
+      
+      
+      $`Organism|ArterialBlood|Plasma|Aciclovir`
+      $`Organism|ArterialBlood|Plasma|Aciclovir`$xRange
+      [1]  -72 1512  -72 1512  -72 1512
+      
+      $`Organism|ArterialBlood|Plasma|Aciclovir`$yRange
+      [1] -3.463  2.839 -3.463  2.839 -3.463  2.839
+      
+      
+      $`Organism|PeripheralVenousBlood|Aciclovir|Plasma (Peripheral Venous Blood)`
+      $`Organism|PeripheralVenousBlood|Aciclovir|Plasma (Peripheral Venous Blood)`$xRange
+      [1]  -72 1512  -72 1512  -72 1512
+      
+      $`Organism|PeripheralVenousBlood|Aciclovir|Plasma (Peripheral Venous Blood)`$yRange
+      [1] -6.1814 -0.3824 -6.1814 -0.3824 -6.1814 -0.3824
+      
+      
 
--- tests/testthat/test-sensitivity-time-profiles.R
+++ tests/testthat/test-sensitivity-time-profiles.R
@@ -87,10 +87,7 @@ test_that("sensitivityTimeProfiles correctly applies linear y-axis scaling", {
     )
   )
 
-  pbs <- purrr::map(seq_along(p[[n]]), ~ ggplot2::ggplot_build(p[[n]][[.x]]))
-  plotParams <- purrr::map(pbs, ~ .x$layout$panel_params[[1]]$y.range)
-
-  expect_snapshot(unlist(plotParams))
+  expect_snapshot(extractAxisRange(p))
 })
 
 test_that("sensitivityTimeProfiles plots correctly with inclusion of observed data", {
@@ -103,6 +100,56 @@ test_that("sensitivityTimeProfiles plots correctly with inclusion of observed da
       title = "sensitivityTimeProfiles works with observed data",
       fig = p
     )
+  )
+})
+
+
+# unit conversion --------------------------------------------
+
+test_that("sensitivityTimeProfiles throws an error for non-list units", {
+  expect_error(
+    sensitivityTimeProfiles(results, xUnits = "h"),
+    "argument 'xUnits' is of type 'character', but expected 'list'"
+  )
+  expect_error(
+    sensitivityTimeProfiles(results, yUnits = "mol/l"),
+    "argument 'yUnits' is of type 'character', but expected 'list'"
+  )
+})
+
+test_that("sensitivityTimeProfiles throws an error if unit is not valid", {
+  # invalid unit
+  expect_error(
+    sensitivityTimeProfiles(results, yUnits = list("mol/kg")),
+    "Value 'mol/kg' is not in defined enumeration values"
+  )
+  # invalid x-axis unit
+  expect_error(
+    sensitivityTimeProfiles(results, xUnits = list("mol/l")),
+    "Value 'mol/l' is not in defined enumeration values"
+  )
+})
+
+test_that("sensitivityTimeProfiles plots correctly apply unit conversion", {
+  set.seed(123)
+  p <- sensitivityTimeProfiles(results, xUnits = list("h"), yUnits = list("mol/l"))
+
+  set.seed(123)
+  suppressWarnings(
+    vdiffr::expect_doppelganger(
+      title = "sensitivityTimeProfiles unit conversion",
+      fig = p
+    )
+  )
+})
+
+test_that("sensitivityTimeProfiles plots handle y-unit conversion correctly when dimensions are not convertible", {
+  p1 <- sensitivityTimeProfiles(results) # default
+  p2 <- sensitivityTimeProfiles(results, yUnits = list("mol")) # no conversion
+
+  expect_identical(
+    extractAxisRange(p1),
+    extractAxisRange(p2)
   )
 })
 
@@ -149,6 +196,45 @@ test_that("sensitivityTimeProfiles plots correctly for multiple outputs with obs
       title = "multiple output path profiles with observed data",
       fig = plotsMultiple
     )
+  )
+})
+
+
+# multiple output paths unit conversion
+
+test_that("sensitivityTimeProfiles plots correctly apply y-unit conversion for multiple paths", {
+  p <- sensitivityTimeProfiles(resultsMultiple,
+    yUnits = list("mol/l", "month(s)", "nmol")
+  )
+
+  expect_snapshot(extractAxisRange(p))
+})
+
+test_that("sensitivityTimeProfiles plots correctly apply y-unit conversion for multiple paths with `NULL`", {
+  p <- sensitivityTimeProfiles(resultsMultiple,
+    yUnits = list("mol/l", NULL, "mol")
+  )
+
+  expect_snapshot(extractAxisRange(p))
+})
+
+test_that("sensitivityTimeProfiles plots correctly apply y-unit conversion for multiple paths with single unit", {
+  p <- sensitivityTimeProfiles(resultsMultiple,
+    yUnits = list("mg/ml")
+  )
+
+  expect_snapshot(extractAxisRange(p))
+})
+
+test_that("sensitivityTimeProfiles plots handle y-unit conversion correctly for multiple paths when dimensions are not convertible", {
+  p1 <- sensitivityTimeProfiles(resultsMultiple) # default
+  p2 <- sensitivityTimeProfiles(resultsMultiple, # not converted
+    yUnits = list("mol", "kg", "µmol/h")
+  )
+
+  expect_identical(
+    extractAxisRange(p1),
+    extractAxisRange(p2)
   )
 })
 
@@ -210,6 +296,7 @@ test_that("sensitivityTimeProfiles plots correctly for multiple outputs with mul
     )
   )
 })
+
 
 # filter data to be plotted -------------------------------------
 
