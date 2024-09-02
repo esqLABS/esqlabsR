71879c10169cc2e5dde83945c37f098b09bfc2bc
Felix MIL <felix.mil@esqlabs.com>
2024-09-02 14:31:22 +0200

    Remove PKSIM installation steps + remove rClr installation steps

--- inst/extdata/install-packages.R
+++ inst/extdata/install-packages.R
@@ -12,7 +12,6 @@ To enable long path, follow instructions at this adress: https://www.microfocus.
   }
 }
 
-
 install_script_deps <- function(install_option) {
   # install dependencies required for this script
 
@@ -57,71 +56,46 @@ install_script_deps <- function(install_option) {
       )
     }
   }
-  cli_alert_success("Install prerequisite dependencies")
+  cli::cli_alert_success("Install prerequisite dependencies")
 }
 
 check_RTools <- function() {
-  cli_progress_step("Verify RTools installation")
+  cli::cli_progress_step("Verify RTools installation")
   if (!pkgbuild::find_rtools()) {
-    cli_alert_danger("Rtools is not installed. Please install it before continuing.")
-    cli_alert_info("You can download Rtools from https://cran.r-project.org/bin/windows/Rtools/")
-    cli_alert_info("Please install the latest version of Rtools.")
-    cli_alert_info("After installing Rtools, please restart R and run this script again.")
+    cli::cli_alert_danger("Rtools is not installed. Please install it before continuing.")
+    cli::cli_alert_info("You can download Rtools from https://cran.r-project.org/bin/windows/Rtools/")
+    cli::cli_alert_info("Please install the latest version of Rtools.")
+    cli::cli_alert_info("After installing Rtools, please restart R and run this script again.")
     stop("Rtools is not installed.")
   }
-  cli_progress_done(result = "done")
-}
-
-get_PKSim_Minimal <- function(install_option) {
-  # Only for local installation
-  if (install_option == 1) {
-    cli_progress_step("Get minimal version of PKSim.")
-
-    download.file("https://ci.appveyor.com/api/projects/open-systems-pharmacology-ci/ospsuite-r/artifacts/pksim_minimal.zip",
-      destfile = "pksim_minimal.zip"
-    )
-    unzip("pksim_minimal.zip", exdir = "PKSim")
-    file.remove("pksim_minimal.zip")
-
-    cli_progress_step("Verify PKSim installation")
-    # test if PK-Sim.R dll can be loaded
-    ospsuite::initPKSim(pksimFolderPath = "PKSim")
-
-    cli_progress_done(result = "done")
-  }
+  cli::cli_progress_done(result = "done")
 }
 
 get_esqlabsR <- function() {
   # package installation
-  cli_progress_step("Install esqlabsR and dependencies")
+  cli::cli_progress_step("Install esqlabsR and dependencies")
 
-  install.packages("https://github.com/Open-Systems-Pharmacology/rClr/releases/download/v0.9.2/rClr_0.9.2.zip",
-    repos = NULL,
-    type = "binary",
-    quiet = TRUE
-  )
   remotes::install_github("esqLABS/esqlabsR@*release",
     build = TRUE,
     upgrade = "always"
   )
 
-  cli_progress_done(result = "done")
+  cli::cli_progress_done(result = "done")
 }
 
 load_esqlabsR <- function() {
-  cli_progress_step("Load esqlabsR")
-  unloadNamespace(ns = "esqlabsR")
-  unloadNamespace(ns = "ospsuite.parameteridentification")
+  cli::cli_progress_step("Load esqlabsR")
+
   # test if installed packages can be loaded
   library("esqlabsR")
 
-  cli_progress_done(result = "done")
+  cli::cli_progress_done(result = "done")
 }
 
 run_test_simulation <- function() {
   # Run test simulation to verify installation
 
-  cli_progress_step("Run test simulation.")
+  cli::cli_progress_step("Run test simulation.")
 
   tryCatch(
     expr = {
@@ -146,12 +120,12 @@ run_test_simulation <- function() {
 
       simulatedScenarios <- runScenarios(scenarios = scenarios)
 
-      cli_progress_done(result = "done")
-      cli_alert_success("Installation successful.")
+      cli::cli_progress_done(result = "done")
+      cli::cli_alert_success("Installation successful.")
     },
     error = function(error) {
-      cli_progress_done(result = "failed")
-      cli_alert_danger(text = paste("Simulation test failed: ", error))
+      cli::cli_progress_done(result = "failed")
+      cli::cli_alert_danger(text = paste("Simulation test failed: ", error))
     }
   )
 }
@@ -162,10 +136,10 @@ initialize_project <- function() {
   )
 
   if (initialize_option == 1) {
-    cli_progress_step("Initialize project structure")
+    cli::cli_progress_step("Initialize project structure")
     init_project(destination = "../")
     update_project_conf()
-    cli_progress_done(result = "done")
+    cli::cli_progress_done(result = "done")
   }
 }
 
@@ -222,10 +196,10 @@ restart_rstudio <- function() {
   rm(list = ls(envir = .GlobalEnv), envir = .GlobalEnv)
   # if running in RStudio, restart R session
   if (isAvailable()) {
-    cli_alert_info("Restarting R session.")
+    cli::cli_alert_info("Restarting R session.")
     restartSession(command = "library(esqlabsR)")
   } else {
-    cli_alert_info("Please restart R to complete the installation.")
+    cli::cli_alert_info("Please restart R to complete the installation.")
   }
 }
 
@@ -242,8 +216,6 @@ setup_esqlabsR <- function() {
   check_RTools()
 
   get_esqlabsR()
-
-  get_PKSim_Minimal(install_option)
 
   load_esqlabsR()
 
