2bbf9f22e718e1e232c74c78c86fb2292381e31a
Felix MIL <dev@felixmil.com>
2025-03-24 09:35:06 +0100

    styler::style_pkg()

--- tests/testthat/test-utilities-population.R
+++ tests/testthat/test-utilities-population.R
@@ -115,8 +115,8 @@ test_that("extendPopulationFromXLS works", {
       .writeExcel(
         path = PopulationParameters,
         data = list("UserDefinedVariability" = data.frame(
-          `Container Path` = c("Organism|Kidney","Organism|Kidney"),
-          `Parameter Name` = c("GFR","eGFR"),
+          `Container Path` = c("Organism|Kidney", "Organism|Kidney"),
+          `Parameter Name` = c("GFR", "eGFR"),
           "Mean" = 0.12,
           "SD" = 0.001,
           "Distribution" = "Normal",
@@ -134,10 +134,12 @@ test_that("extendPopulationFromXLS works", {
       expect_snapshot(
         population$getParameterValuesForIndividual(4)
       )
-expect_equal(population$allParameterPaths, c("Organism|PeripheralVenousBlood|Aciclovir|Plasma (Peripheral Venous Blood)",
-                                             "Organism|Muscle|Intracellular|Aciclovir|Concentration",
-                                             "Organism|Kidney|GFR",
-                                             "Organism|Kidney|eGFR"))
+      expect_equal(population$allParameterPaths, c(
+        "Organism|PeripheralVenousBlood|Aciclovir|Plasma (Peripheral Venous Blood)",
+        "Organism|Muscle|Intracellular|Aciclovir|Concentration",
+        "Organism|Kidney|GFR",
+        "Organism|Kidney|eGFR"
+      ))
     }
   )
 })
