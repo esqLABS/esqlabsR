a364de90b13fa3307aca5ef664f909f1de9b31ba
Felix MIL <dev@felixmil.com>
2025-09-19 12:40:24 +0200

    forgot to commit this in 9d64d7234bf1cbe77634f30a4d38e02add58441b

--- R/utilities-scenario-configuration.R
+++ R/utilities-scenario-configuration.R
@@ -1527,12 +1527,12 @@ addScenarioConfigurationsToExcel <- function(
 #'   and multiple intervals are separated by semicolons.
 #'
 #' @details Parses a string representation of simulation time intervals into a list
-#' of numeric vectors. Each vector contains three elements: [start_time, end_time, resolution].
+#' of numeric vectors. Each vector contains three elements: start_time, end_time, resolution.
 #' The function validates that all values are numeric, positive, and that start times
 #' are less than end times.
 #'
 #' @returns A list of numeric vectors, each containing three elements representing
-#' [start_time, end_time, resolution] for each time interval. Returns `NULL` if
+#' start_time, end_time, resolution for each time interval. Returns `NULL` if
 #' the input string is `NULL`.
 #'
 #' @keywords internal
