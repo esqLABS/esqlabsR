19b1c9a310a12a3f284b5e2c92c5e99f970ff8a4
pchelle <chelle.pierre@gmail.com>
2026-04-08 12:39:38 -0400

    Export configuration independent from tlf

--- R/esqlabs-plot-export-configuration.R
+++ R/esqlabs-plot-export-configuration.R
@@ -1,17 +1,17 @@
 #' @title ExportConfiguration
-#' @description R6 class extending [tlf::ExportConfiguration] with row-aware
-#'   height.
-#'
-#' @details Inherits fields and behavior from [tlf::ExportConfiguration].
-#'
-#' @section Inherited fields: See [tlf::ExportConfiguration] for `name`, `path`,
-#'   `format`, `width`, `height`, `units`, and `dpi`.
-#'
+#' @description R6 class defining and managing how to export/save ggplot objects
+#' with row-aware height.
+#' @field fileName character defining the name of the file to be saved
+#' @field format character defining the format/device of the file to be saved
+#' @field width numeric values defining the width in `units` of the plot dimensions after saving
+#' @field height numeric values defining the height in `units` of the plot dimensions after saving
+#' @field units character defining the unit of the saving dimension
+#' @field dpi (dots per inch) numeric value defining plot resolution
 #' @family PlotConfiguration classes
 #' @export
 ExportConfiguration <- R6::R6Class(
   "ExportConfiguration",
-  inherit = tlf::ExportConfiguration,
+  cloneable = FALSE,
   active = list(
     #' @field heightPerRow The height of the plot dimensions for a row in a
     #'   multi panel plot. The final height of the figure will be 'heightPerRow'
@@ -32,6 +32,59 @@ ExportConfiguration <- R6::R6Class(
     .height = NULL
   ),
   public = list(
+    fileName = NULL,
+    format = NULL,
+    width = NULL,
+    height = NULL,
+    units = NULL,
+    dpi = NULL,
+
+    #' @description Create a new `ExportConfiguration` object
+    #' @param fileName character defining the name of the file to be saved
+    #' @param format character defining the format of the file to be saved
+    #' @param width numeric values defining the width in `units` of the plot dimensions after saving
+    #' @param height numeric values defining the height in `units` of the plot dimensions after saving
+    #' @param units character defining the unit of the saving dimension
+    #' @param dpi numeric value defining plot resolution (dots per inch)
+    #' @return A new `ExportConfiguration` object
+    initialize = function(fileName = NULL,
+                          format = NULL,
+                          width = NULL,
+                          height = NULL,
+                          units = NULL,
+                          dpi = NULL) {
+      validateIsCharacter(fileName, nullAllowed = TRUE)
+      validateIsCharacter(format, nullAllowed = TRUE)
+      validateIsIncluded(units, c("cm", "in", "mm", "px"), nullAllowed = TRUE)
+      validateIsNumeric(width, nullAllowed = TRUE)
+      validateIsNumeric(height, nullAllowed = TRUE)
+      validateIsNumeric(dpi, nullAllowed = TRUE)
+
+      self$format <- format %||% ospsuite.plots::getOspsuite.plots.option(ospsuite.plots::OptionKeys$exportDevice)
+      self$width <- width %||% ospsuite.plots::getOspsuite.plots.option(ospsuite.plots::OptionKeys$exportWidth)
+      # Height is not defined by ospsuite.plots, using width to create square plots
+      self$height <- height %||% ospsuite.plots::getOspsuite.plots.option(ospsuite.plots::OptionKeys$exportWidth)
+      self$units <- units %||% ospsuite.plots::getOspsuite.plots.option(ospsuite.plots::OptionKeys$exportUnits)
+      self$dpi <- dpi %||% ospsuite.plots::getOspsuite.plots.option(ospsuite.plots::OptionKeys$exportDpi)
+      self$fileName <- fileName %||% "figure"
+      return(invisible())
+    },
+
+    #' @description Print properties of export configuration
+    #' @return Export configuration properties
+    print = function() {
+      ospsuite.utils::ospPrintItems(
+        list(
+          "Format/Device" = self$format,
+          "Width" = paste(self$width, self$units),
+          "Height Per Row" = paste(self$width, self$units),
+          "Resolution" = paste(self$dpi, " dots per inch")
+        ),
+        title = "Export Configuration"
+      )
+      return(invisible())
+    },
+
     #' @description Save/Export a plot
     #' @param plotObject A `ggplot` object
     #' @param fileName character file name of the exported plot
@@ -52,7 +105,14 @@ ExportConfiguration <- R6::R6Class(
         )[[1]]
         self$height <- self$heightPerRow * nrOfRows
       }
-      super$savePlot(plotObject, fileName)
+      ospsuite.plots::exportPlot(
+        plotObject,
+        filename = basename(fileName %||% self$path),
+        filepath = dirname(fileName %||% self$path),
+        width = self$width,
+        height = self$height,
+        device = self$format
+      )
       # Restore the old value
       self$height <- oldHeight
     }
