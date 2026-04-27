710d9a0b995c59b6e9aa608ca5c125f9aa2cfd8f
pchelle <chelle.pierre@gmail.com>
2026-04-27 10:41:52 -0400

    Prevent redundant TESTTHAT_PARALLEL

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,7 +7,6 @@ options(warnPartialMatchDollar = FALSE)
 
 numCores <- parallel::detectCores()
 numCores <- if (is.na(numCores)) 1L else numCores
-Sys.setenv(TESTTHAT_PARALLEL = "true")
 Sys.setenv(TESTTHAT_CPUS = as.character(numCores))
 
 test_check("esqlabsR")
