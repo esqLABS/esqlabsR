b8459bced8f44b112decb5f7b4e65d42cfda23ac
pchelle <chelle.pierre@gmail.com>
2026-04-27 10:50:54 -0400

    Remove CPUS hardcoded definition

--- tests/testthat.R
+++ tests/testthat.R
@@ -5,8 +5,4 @@ library(ospsuite.utils)
 # Disable these warnings for the tests
 options(warnPartialMatchDollar = FALSE)
 
-numCores <- parallel::detectCores()
-numCores <- if (is.na(numCores)) 1L else numCores
-Sys.setenv(TESTTHAT_CPUS = as.character(numCores))
-
 test_check("esqlabsR")
